71439819c5007ed984648858fb3f9d20e973bea8
alga23 <algarcac3@gmail.com>
2023-09-29 15:18:59 +0200

    Arreglos de archivos

--- M13_Practica2_MWC/src/main/java/com/mycompany/m13_practica2_mwc/Tablet.java
+++ M13_Practica2_MWC/src/main/java/com/mycompany/m13_practica2_mwc/Tablet.java
@@ -1,6 +1,6 @@
 package com.mycompany.m13_practica2_mwc;
 
-//Classe tablet
+//Classe tablet que extiende de la Clase Dispositivos e implementa la interfaz GammaAlta
 
 public class Tablet extends Dispositivos implements GammaAlta {
     private int polzades;
@@ -25,14 +25,22 @@ public class Tablet extends Dispositivos implements GammaAlta {
         this.polzades = polzades;
     }
 
+    //metodo que nos indica el precio final,
+    //multiplicando por  el precio base 
     public Double preuFinal() {
         return getPreuBase() * 2;
     }
 
+    //metodo que nos indica si el dispositivo
+    //es gama alta. Pra hacer esto, vemos si el
+    //precio es mayor a 900 (true), lo es. Si es
+    //mas pequeño, no lo es
     public Boolean isGammaAlta() {
         return preuFinal() > 900;
     }
 
+    //Nos devuelve las pulgadas que tiene
+    //una tablet en una frase
     @Override
     public String toString() {
         return "Tablet [polzades=" + polzades + "]";
